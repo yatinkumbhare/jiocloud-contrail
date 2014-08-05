bedbe3b8f35b49afdb726ab4138be15773065d09
hkumarmk <hkumarmk@gmail.com>
2014-08-05 07:41:01 +0000

    added contrail dependancy packages
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/hkumarmk-contrail@876 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,4 @@
 class contrail::params {
   $package_ensure = present
+  $contrail_dep_packages = ['binutils','libdpkg-perl', 'make', 'patch', 'python-pip', 'python-pkg-resources', 'python-setuptools', 'libexpat1','python-crypto','python-netaddr','python-paramiko','contrail-fabric-utils']
 }
