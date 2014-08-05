eb9e7588cca9fd1b27127ed7f92373d90c6dc2dd
hkumarmk <hkumarmk@gmail.com>
2014-08-05 19:21:06 +0000

    Merge branch 'master' into restructure_code_to_puppet_way
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/hkumarmk-contrail@881 7e7508ed-7dfa-4282-9f22-b7e683a64a08

---
+++ contrail/files/setup_rabbitmq_cluster.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -x
+ostype=$1;shift
+uuid=$1;shift
+master=$1;shift
+is_master=$1;shift
+#TODO Ppvide support for centos
+
+this_host=$(hostname)
+
+
+rabbitmqctl cluster_status | grep $master
+master_added=$?
+rabbitmqctl cluster_status | grep $this_host
+slave_added=$?
+
+existing_uuid=$(cat /var/lib/rabbitmq/.erlang.cookie)
+
+if [ $is_master == "yes" ] && [ $existing_uuid == $uuid ]; then
+    exit 0
+fi
+
+if [ $is_master == "no" ] && [ $master_added == 0 ] && [ $slave_added == 0 ]; then
+    exit 0
+fi
+
+#Stop other cfgm services,
+#they will will all be restarted in config_server-setup.sh
+
+service supervisor-config status | grep running
+supervisor_config_running=$?
+if [ supervisor_config_running != 0 ]; then
+    eval "service supervisor-config start"
+    eval "supervisorctl -s http://localhost:9004 stop all"
+fi
+
+#setup and start rabbitmq
+eval "sudo ufw disable"
+eval "rm -rf /var/lib/rabbitmq/mnesia"
+
+eval "service rabbitmq-server stop"
+eval "epmd -kill"
+echo ${uuid} > /var/lib/rabbitmq/.erlang.cookie
+
+
+eval "service rabbitmq-server start"
+
+#Print the cluste status
+eval "rabbitmqctl cluster_status"
+
+
