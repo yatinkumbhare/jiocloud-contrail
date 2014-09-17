5f9655bcbb1c2f59d57cae42281cae5652c1e18c
Harish Kumar <hkumarmk@gmail.com>
2014-09-17 08:29:54 +0000

    Removed all unncessary files from contrail module

--- manifests/params.pp
+++
@@ -1,4 +0,0 @@
-class contrail::params {
-  $package_ensure = present
-  $contrail_dep_packages = ['binutils','libdpkg-perl', 'make', 'patch', 'python-pip', 'python-pkg-resources', 'python-setuptools', 'libexpat1','python-crypto','python-netaddr','python-paramiko','contrail-fabric-utils']
-}
--- manifests/rabbitmq.pp
+++
@@ -1,8 +0,0 @@
-##
-class contrail::rabbitmq {
-  class {'::rabbitmq':
-    port => 5672,
-    manage_repos => false,
-    admin_enable => false,
-  }
-}
