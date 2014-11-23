5a5cc5e6965bc3c8215fff7fb7089cea1c826d0e
hkumarmk <hkumar@d4devops.org>
2014-11-23 18:32:07 +0530

    Handle vgw provisioning persistant and fault tollerant
    This patch will fix inconsistancies on vgw provisioning and to make this
    persistent by adding interface configuration for vgw and using vrouter agent to
    manage the route configuration.
    
    1. provision-vgw-interface is sometimes leaves vgw interface unstable. Also this
    is not persistant. So not using it, rather run individual commands from puppet,
    make interface configuration persistant, use contrail vrouter agent to register
    the route to contrail system.
    2. Added puppet type to configure vrouter agent rather than using templates
    3. Added a defined type to provision vgw.
    
    Note: Spec tests are WIP (not pushed) - I faced an wierd error, need to refer to Dan.

---
+++ manifests/vgw.pp
@@ -0,0 +1,59 @@
+#
+# Define: contrail::vgw
+#   To provision contrail vgw interfaces
+#
+define contrail::vgw (
+  $subnet,
+  $vrf,
+  $dest_net = '0.0.0.0/0',
+) {
+
+  validate_re($title, 'vgw\d+')
+
+  $interface = $title
+
+  include contrail::vrouter
+
+  ##
+  # Enable ip forwarding
+  ##
+  ensure_resource('sysctl::value','net.ipv4.ip_forward',{'value' => 1})
+
+  ##
+  # Create vif.
+  ##
+  exec {"vif_create_${interface}":
+    command => "vif --create ${interface} --mac 00:01:00:5e:00:00",
+    unless  => "vif --list | grep ${interface}",
+    notify  => Service['contrail-vrouter-agent']
+  }
+
+  ##
+  # This step make vgw configuration persistant.
+  # TODO: Not handling multiple subnets here. This will be look into at later point.
+  ##
+  network_config { $interface:
+    ensure    => present,
+    onboot    => true,
+    method    => 'manual',
+    options   =>  { 'pre-up' => "vif --create ${interface} --mac 00:01:00:5e:00:00",
+                    'up'     => "route add -net ${subnet} dev ${interface}"
+                  },
+    require   => Exec["vif_create_${interface}"]
+  }
+
+  exec {"ifup_${interface}":
+    command => "ifup ${interface}",
+    unless  => "ifconfig | grep ${interface}",
+    require => Network_config[$interface],
+  }
+
+  $gw_num = regsubst($interface,'vgw(\d+)','\1')
+
+  contrail_vrouter_config {
+    "GATEWAY-${gw_num}/interface":        value => $interface;
+    "GATEWAY-${gw_num}/routing_instance": value => $vrf;
+    "GATEWAY-${gw_num}/ip_blocks":        value => $subnet;
+    "GATEWAY-${gw_num}/routes":           value => $dest_net
+  }
+}
